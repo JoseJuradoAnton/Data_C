059146767517ad260353c36a1f9b1c57c9129324
JoseJuradoAnton <pythonhack22@gmail.com>
2025-01-28 05:18:40 -0500

    first commit

---
+++ helloWord.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main(){
+    std::cout<<"hello c2c mundo"<<'\n';
+    std::cout<<"malware 2";
+    return 0;
+}
+// un comentario de una limea
+
+/* 
+varios comentarios
+varias lineas
+1
+2
+*/
---
+++ variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+  namespace primero {
+    int x = 150;
+
+  }
+
+int main() {
+    /*  
+    int x = 3;
+    std::cout<<"X = "<<x;
+    */
+   std::cout<<"X = "<<primero::x;
+  return 0;
+}
